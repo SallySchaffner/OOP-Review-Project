d6062242705ff3019b8bb6b69b9ebe124b13f2a5
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-08-11 16:13:43 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// OOP Review Project.cpp : This file contains code for three classes, the PointType class, the Circle Class, and the CylinderType class. 
+// The PointType class is developed on a branch.
+// The CircleType class is on a separate branch that is based off the PointType branch.
+// The CylinderType class is on a third branch that is based off the CirclType branch. 
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "OOP Review Project\n";
+}
